b32fbd1b3623ffb62ebe3bda8a13eb5e90617eca
kharioki <tony.kharioki@gmail.com>
2023-03-16 15:16:02 +0300

    withTrpc HOC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,25 @@ import '@fontsource/poppins'
 import type { AppType } from 'next/app';
 import type { ServerRouter } from '@/server/router'
 import { createTRPCNext } from '@trpc/next';
+import { httpBatchLink } from '@trpc/client';
+
+function getBaseUrl() {
+  if (typeof window === 'undefined') {
+    return process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : `http://localhost:3000/api/trpc`
+  }
+  return '/api/trpc'
+}
 
 const { withTRPC } = createTRPCNext<ServerRouter>({
   config({ ctx }) {
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : `http://localhost:3000/api/trpc`
-
-    return { url };
+    const links = [
+      httpBatchLink({
+        url: getBaseUrl(),
+      }),
+    ];
+    return { links };
   },
   ssr: true,
 });
